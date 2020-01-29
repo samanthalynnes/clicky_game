bf29644267dc1900849ce18ed42433f44fbd6b89
Samantha Lynnes <samanthalynnes522@gmailcom>
2020-01-28 21:50:45 -0600

    Updates

---
+++ precache-manifest.e6842269bead4e54d34f392eaab6e510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5f6674176ead2be302761e049e1cf9",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "4b629a96893ac50d002e",
+    "url": "/clicky_game/static/css/main.3292fe79.chunk.css"
+  },
+  {
+    "revision": "571ad056e4cbc6252bdd",
+    "url": "/clicky_game/static/js/2.ac865da5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky_game/static/js/2.ac865da5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b629a96893ac50d002e",
+    "url": "/clicky_game/static/js/main.47033e52.chunk.js"
+  },
+  {
+    "revision": "98a991019910cbc1107f",
+    "url": "/clicky_game/static/js/runtime-main.efcabb2a.js"
+  }
+]);
